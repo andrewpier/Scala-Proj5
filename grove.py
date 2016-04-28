ea6299e53ca5e5931deb9878f90e651eec7bd5dc
Andrew Pier <PIERAK1@GCC.EDU>
2016-04-27 20:54:31 -0400

    more language stuff

--- grove.py
+++ grove.py
@@ -1,8 +1,11 @@
 exec(open("grove_parse.py").read())
 
 while True:
-    ln = input("Grove>> ")
-    root = parse(ln)
-    res = root.eval()
-    if not res is None:
-        print(res)
+    try:
+        ln = input("Grove>> ")
+        root = parse(ln)
+        res = root.eval()
+        if not res is None:
+            print(res)
+    except:
+        raise GroveError("ERROR!!")
--- grove_lang.py
+++ grove_lang.py
@@ -1,5 +1,6 @@
 exec(open("GroveError.py").read())
 import sys
+import importlib
 var_table = {}
 
 class Expr:
@@ -7,18 +8,19 @@ class Expr:
 
 class StringLiteral:
     def __init(self,name):
-        if(name[0].isalpha() or name[0] == '_' and name[1:].isalnum()):
+        if not (" "  in name) and not("." is in name):
             self.name = name
         else:
             raise GroveError("GROVE: Improper string literal")
-        
-        
     def eval(self):
-        return "\"" + self.name + "\""
+        return self.name
 
 class Name(Expr):
     def __init__(self,name):
-        self.name = name
+        if (name[0].isalpha or name[0] == "_" and name[1:].isalnum() ):
+            self.name = name
+        else:
+            raise GroveError("variable anme in correct")
     
     def getName(self):
         return self.name
@@ -31,6 +33,43 @@ class Name(Expr):
         else:
             raise GroveError("error?")
             
+class Method(Expr):
+    def __init__(self,keyWord, firstName,secName, *args):
+        self.keyWord = keyWord
+        self.firstName = firstName
+        self.secName = secName
+    def __eval__(self):
+         if self.firstName in var_table:
+            methodList = [methodName for methodName in dir(self.firstName) if callable(getattr(self.firstName,methodName))]
+            if self.secondName in methodList:
+                #not sure what goes here
+                self.firstName.eval()
+            else:
+                raise GroveError("GROVE: method " + str(self.secondName) + " is not defined for: " + str(self.firstName) )
+        else:
+            raise GroveError("GROVE: name not found in var table, METHOD")
+        
+            
+            
+class Addition(Expr):
+    
+    def __init__(self,child1,child2):
+        self.child1 = child1
+        self.child2 = child2
+        
+        if not isinstance(self.child1,Expr):
+            raise GroveError("CALC: expected expression but recieved " + str(type(self.child1)))
+            
+        if not isinstance(self.child2,Expr):
+            raise GroveError("CALC: expected expression but recieved " + str(type(self.child2)))
+            
+    def eval(self):
+        if not isinstance(self.child2,self.child1):
+            raise GroveError("GROVE: expected" + str(type(self.child1) + " but found " + str(type(self.child2))
+        else:
+            return self.child1.eval() + self.child2.eval()
+    
+    
             
 class Num(Expr):
     def __init__(self,value):
@@ -53,5 +92,14 @@ class Stmt:
     def eval(self):
         if(self.name == "quit" || self.name == "exit"):
             sys.exit()
-            
-        var_table[self.name.getName()] = self.value.eval()
+        elif self.name == "import":
+            module = importlib.import_module(self.value)
+            #call = getattr(mod,)
+            #call()
+        else:
+            var_table[self.name.getName()] = self.value.eval()
+                                                     
+                                                     
+                                                     
+                                                     
+                                                     
